fca0f10425605d4035879aa8407f8200d4dde613
mcunlocked <146917292+mcunlocked@users.noreply.github.com>
2023-10-03 22:56:34 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+function unboxed1() {
+        document.getElementById('boxed1')
+        .style.display = "inline-block";
+        document.getElementById('unboxed1')
+        .style.display = "none";
+  } 
+  function boxed1() {
+    document.getElementById('unboxed1')
+    .style.display = "inline-block";
+    document.getElementById('boxed1')
+    .style.display = "none";
+} 
+function unboxed2() {
+    document.getElementById('boxed2')
+    .style.display = "inline-block";
+    document.getElementById('unboxed2')
+    .style.display = "none";
+} 
+function boxed2() {
+document.getElementById('unboxed2')
+.style.display = "inline-block";
+document.getElementById('boxed2')
+.style.display = "none";
+} 
+function unboxed3() {
+    document.getElementById('boxed3')
+    .style.display = "inline-block";
+    document.getElementById('unboxed3')
+    .style.display = "none";
+} 
+function boxed3() {
+document.getElementById('unboxed3')
+.style.display = "inline-block";
+document.getElementById('boxed3')
+.style.display = "none";
+} 
+function unboxed4() {
+    document.getElementById('boxed4')
+    .style.display = "inline-block";
+    document.getElementById('unboxed4')
+    .style.display = "none";
+} 
+function boxed4() {
+document.getElementById('unboxed4')
+.style.display = "inline-block";
+document.getElementById('boxed4')
+.style.display = "none";
+} 
+function unboxed5() {
+    document.getElementById('boxed5')
+    .style.display = "inline-block";
+    document.getElementById('unboxed5')
+    .style.display = "none";
+} 
+function boxed5() {
+document.getElementById('unboxed5')
+.style.display = "inline-block";
+document.getElementById('boxed5')
+.style.display = "none";
+} 
